9e4035554934c918a45e94b3cd028e247ea05c62
JJongW <71203867+JJongW@users.noreply.github.com>
2022-09-04 00:33:40 +0900

    gogo

---
+++ test.py
@@ -0,0 +1,31 @@
+n=26
+print(n/10%10)
+k=[]
+k.append(n%10)
+n1=n-k[0]
+n1=int(n1/10)
+k.append(n1%10)
+temp=k[0]
+k[0]=k[1]
+k[1]=temp
+print(k)
+n2=k[0]+k[1]
+if n2>9:
+    n2=n2%10
+n=k[1]*10+n2
+print(n)
+print(n/10%10)
+k=[]
+k.append(n%10)
+n1=n-k[0]
+n1=int(n1/10)
+k.append(n1%10)
+temp=k[0]
+k[0]=k[1]
+k[1]=temp
+print(k)
+n2=k[0]+k[1]
+if n2>9:
+    n2=n2%10
+n=k[1]*10+n2
+print(n)
